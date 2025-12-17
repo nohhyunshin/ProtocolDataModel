e9adc9e9e3b67f4bd5b8bcdefebdd693de8d6a71
노현신 <gustls0709@gmail.com>
2025-12-17 12:59:33 +0900

    all protocol model - devices

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time
-from protocol.ProtocolDevices import ModbusTCP, OPCUA, RS485,ProtocolDevices
+from protocol.ProtocolDevices import ModbusTCP, OPCUA, RS485
 from protocol.ExceptionHandling import error_exception
 from protocol.StandardLogic import to_standard_format
 from protocol.MQTTPublish import publish_mqtt
@@ -13,16 +13,19 @@ devices = [
 for device in devices :
     device.connect()
 
-while True :
+try :
+    while True :
+        for device in devices :
+            try :
+                raw = error_exception(device)
+                meta = device.get_metadata()
+                standard = to_standard_format(raw,meta)
+                publish_mqtt(standard)
+                print(standard)
+            except Exception as e :
+                print(e)
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("프로그램 종료")
     for device in devices :
-        try :
-            raw = error_exception(device)
-            meta = device.get_metadata()
-            standard = to_standard_format(raw,meta)
-            publish_mqtt(publish_mqtt.mqtt_client, standard)
-        except Exception as e :
-            print(e)
-        except KeyboardInterrupt:
-            print("프로그램 종료")
-    time.sleep(1)
-    
+        device.disconnect()   
